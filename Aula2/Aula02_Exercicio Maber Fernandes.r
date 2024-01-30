99419d33368d812065d3592c7fefd2336b97b338
Maber Fernandes <71026050+maberf@users.noreply.github.com>
2024-01-29 23:43:49 -0300

    Update Aula02_Exercicio Maber Fernandes.r

--- Aula2/Aula02_Exercicio Maber Fernandes.r
+++ Aula2/Aula02_Exercicio Maber Fernandes.r
@@ -1,22 +1,27 @@
+# FGV Curso Masters - 1o SEMESTRE 2024
+# Disciplina Programação em R
+# Professora Adriana Bezerra Bessa
+# Aluno MABER ATHAYDE FERNANDES
+# Exercício 2 - Manipulação de dados no R: da carga ao processo de transformação
+#
+# Grupo:
 # Gustavo Mazzotti - gmazzotti@mztsolutions.com
 # Maber Fernandes - maberf@gmail.com - maber.fernandes@fgv.edu.br
 # Mariane Kiam - mariane.itocazu@gmail.com
-
+#
+#
 #Limpando o ambiente e o console
 rm(list = ls())
-
 cat("\014")
-
+#
 #Setup do diretório
+setwd("/home/maber/R/Aula2")
 getwd()
-
-setwd("D:\\OneDrive - MZTAD\\coursera\\FGV\\Programacao R\\Aula 2")
-
-dir()
-
+#
+# Carregamento de bibliotecas
 library(readxl)
 library(readr)
-
+#
 GER_PI <- read_excel("DEUPROINDMISMEI.xls", sheet=1, range = "A11:B741")
 head(GER_PI)
 
